a72d8ea5619d69ed6f7f3b69717c1f0b597e4157
Won-Kyu Park <wkpark@gmail.com>
2023-02-24 21:24:42 +0900

    Korean Keyboard Layout Type A/B/C and 103/106 with NLS key support

---
+++ 101/kbd101k.h
@@ -0,0 +1,10 @@
+/****************************** Module Header ******************************\
+* Module Name: kbd101k.h
+\***************************************************************************/
+
+/*
+ * kbd type should be controlled by cl command-line argument
+ */
+#ifndef KBD_TYPE
+#define KBD_TYPE 10
+#endif
